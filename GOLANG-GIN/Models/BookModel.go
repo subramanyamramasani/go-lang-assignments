748c779a51baae6d2e26f6f16612feb844adfe11
ram.subbu9999@gmail.com <ram.subbu9999@gmail.com>
2021-08-06 16:42:52 +0530

    validation on userdata

--- GOLANG-GIN/Models/BookModel.go
+++ GOLANG-GIN/Models/BookModel.go
@@ -1,17 +1,38 @@
 package Models
 
-import "time"
+import (
+	//"github.com/thedevsaddam/govalidator"
+	"fmt"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
 
 type Book struct {
-	Id           uint      `json:"id"`
-	Title        string    `json:"title"`
-	Author       string    `json:"author"`
-	Description  string    `json:"description"`
-	Price        float64   `json:"price"`
-	Isbn         string    `json:"isbn"`
-	Release_date time.Time `json:"release_date"`
+	Id           uint      `validate:"omitempty,uuid" json:"id"`
+	Title        string    `validate:"required" json:"title"`
+	Author       string    `validate:"required" json:"author"`
+	Description  string    `validate:"required" json:"description"`
+	Price        float64   `validate:"required" json:"price"`
+	Isbn         string    `validate:"required" json:"isbn"`
+	Release_date time.Time `validate:"isdafault" json:"release_date"`
 }
 
+func main() {
+	book := Book{
+		Id:          1,
+		Title:       "morning",
+		Author:      "James",
+		Description: "behalf",
+		Price:       200.0,
+		Isbn:        "54638",
+	}
+	validate := validator.New()
+	err := validate.Struct(book)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+}
 func (b *Book) TableName() string {
 	return "book"
 }
